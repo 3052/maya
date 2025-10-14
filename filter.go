aba56c15f394773b6ec3cd7860008f1625653c9a
367 <367@tuta.io>
2025-10-13 21:39:05 -0500

    +   set bool

--- filter.go
+++ filter.go
@@ -10,7 +10,155 @@ import (
    "strings"
 )
 
-func (f Filters) Filter(resp *http.Response, configVar *Config) error {
+func (f *Filter) Set(input string) error {
+   for _, pair := range strings.Split(input, ",") {
+      key, value, found := strings.Cut(pair, "=")
+      if !found {
+         return errors.New("invalid pair format")
+      }
+      var err error
+      switch key {
+      case "bs":
+         _, err = fmt.Sscan(value, &f.BitrateStart)
+      case "be":
+         _, err = fmt.Sscan(value, &f.BitrateEnd)
+      case "h":
+         _, err = fmt.Sscan(value, &f.Height)
+      case "i":
+         f.Id = value
+      case "l":
+         f.Language = value
+      case "r":
+         f.Role = value
+      default:
+         err = errors.New("unknown key")
+      }
+      if err != nil {
+         return err
+      }
+   }
+   return nil
+}
+
+type Filter struct {
+   BitrateEnd   int
+   BitrateStart int
+   Height       int
+   Id           string
+   Language     string
+   Role         string
+}
+
+func (f *Filter) String() string {
+   var out []byte
+   if f.BitrateStart >= 1 {
+      out = fmt.Append(out, "bs=", f.BitrateStart)
+   }
+   if f.BitrateEnd >= 1 {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "be=", f.BitrateEnd)
+   }
+   if f.Height >= 1 {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "h=", f.Height)
+   }
+   if f.Id != "" {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "i=", f.Id)
+   }
+   if f.Language != "" {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "l=", f.Language)
+   }
+   if f.Role != "" {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "r=", f.Role)
+   }
+   return string(out)
+}
+
+const FilterUsage = `be = bitrate end
+bs = bitrate start
+h = height
+i = id
+l = language
+r = role`
+
+func (f *Filter) bitrate_end_ok(rep *dash.Representation) bool {
+   if f.BitrateEnd == 0 {
+      return true
+   }
+   return rep.Bandwidth <= f.BitrateEnd
+}
+
+func (f *Filter) role_ok(rep *dash.Representation) bool {
+   switch f.Role {
+   case "", rep.GetAdaptationSet().GetRole():
+      return true
+   }
+   return false
+}
+
+func (f *Filter) language_ok(rep *dash.Representation) bool {
+   switch f.Language {
+   case "", rep.GetAdaptationSet().Lang:
+      return true
+   }
+   return false
+}
+
+func (f *Filter) id_ok(rep *dash.Representation) bool {
+   switch f.Id {
+   case "", rep.Id:
+      return true
+   }
+   return false
+}
+
+func (f *Filter) height_ok(rep *dash.Representation) bool {
+   switch f.Height {
+   case 0, *rep.Height:
+      return true
+   }
+   return false
+}
+
+func (f *Filters) representation_ok(rep *dash.Representation) bool {
+   for _, value := range f.Values {
+      if !value.bitrate_end_ok(rep) {
+         continue
+      }
+      if value.BitrateStart > rep.Bandwidth {
+         continue
+      }
+      if !value.height_ok(rep) {
+         continue
+      }
+      if !value.id_ok(rep) {
+         continue
+      }
+      if !value.language_ok(rep) {
+         continue
+      }
+      if !value.role_ok(rep) {
+         continue
+      }
+      return true
+   }
+   return false
+}
+
+func (f *Filters) Filter(resp *http.Response, configVar *Config) error {
    if resp.StatusCode != http.StatusOK {
       var data strings.Builder
       resp.Write(&data)
@@ -54,175 +202,32 @@ func (f Filters) Filter(resp *http.Response, configVar *Config) error {
    return nil
 }
 
-type Filters []Filter
+type Filters struct {
+   Values []Filter
+   set bool
+}
 
-func (f Filters) String() string {
-   var b []byte
-   for i, value := range f {
+func (f *Filters) String() string {
+   var out []byte
+   for i, value := range f.Values {
       if i >= 1 {
-         b = append(b, ',')
+         out = append(out, ' ')
       }
-      b = fmt.Append(b, &value)
+      out = fmt.Append(out, "-f ", &value)
    }
-   return string(b)
+   return string(out)
 }
 
-func (f *Filters) Set(data string) error {
-   *f = nil
-   for _, data := range strings.Split(data, ",") {
-      var filterVar Filter
-      err := filterVar.Set(data)
-      if err != nil {
-         return err
-      }
-      *f = append(*f, filterVar)
+func (f *Filters) Set(input string) error {
+   if !f.set {
+      f.Values = nil
+      f.set = true
    }
-   return nil
-}
-
-const FilterUsage = `be = bitrate end
-bs = bitrate start
-h = height
-i = id
-l = language
-r = role`
-
-func (f *Filter) Set(data string) error {
-   cookies, err := http.ParseCookie(data)
+   var value Filter
+   err := value.Set(input)
    if err != nil {
       return err
    }
-   for _, cookie := range cookies {
-      switch cookie.Name {
-      case "bs":
-         _, err = fmt.Sscan(cookie.Value, &f.BitrateStart)
-      case "be":
-         _, err = fmt.Sscan(cookie.Value, &f.BitrateEnd)
-      case "h":
-         _, err = fmt.Sscan(cookie.Value, &f.Height)
-      case "i":
-         f.Id = cookie.Value
-      case "l":
-         f.Language = cookie.Value
-      case "r":
-         f.Role = cookie.Value
-      default:
-         err = errors.New(".Name")
-      }
-      if err != nil {
-         return err
-      }
-   }
+   f.Values = append(f.Values, value)
    return nil
-}
-
-func (f *Filter) String() string {
-   var b []byte
-   if f.BitrateStart >= 1 {
-      b = fmt.Append(b, "bs=", f.BitrateStart)
-   }
-   if f.BitrateEnd >= 1 {
-      if b != nil {
-         b = append(b, ';')
-      }
-      b = fmt.Append(b, "be=", f.BitrateEnd)
-   }
-   if f.Height >= 1 {
-      if b != nil {
-         b = append(b, ';')
-      }
-      b = fmt.Append(b, "h=", f.Height)
-   }
-   if f.Id != "" {
-      if b != nil {
-         b = append(b, ';')
-      }
-      b = fmt.Append(b, "i=", f.Id)
-   }
-   if f.Language != "" {
-      if b != nil {
-         b = append(b, ';')
-      }
-      b = fmt.Append(b, "l=", f.Language)
-   }
-   if f.Role != "" {
-      if b != nil {
-         b = append(b, ';')
-      }
-      b = fmt.Append(b, "r=", f.Role)
-   }
-   return string(b)
-}
-
-type Filter struct {
-   BitrateEnd   int
-   BitrateStart int
-   Height       int
-   Id           string
-   Language     string
-   Role         string
-}
-
-func (f *Filter) bitrate_end_ok(rep *dash.Representation) bool {
-   if f.BitrateEnd == 0 {
-      return true
-   }
-   return rep.Bandwidth <= f.BitrateEnd
-}
-
-func (f *Filter) role_ok(rep *dash.Representation) bool {
-   switch f.Role {
-   case "", rep.GetAdaptationSet().GetRole():
-      return true
-   }
-   return false
-}
-
-func (f *Filter) language_ok(rep *dash.Representation) bool {
-   switch f.Language {
-   case "", rep.GetAdaptationSet().Lang:
-      return true
-   }
-   return false
-}
-
-func (f *Filter) id_ok(rep *dash.Representation) bool {
-   switch f.Id {
-   case "", rep.Id:
-      return true
-   }
-   return false
-}
-
-func (f *Filter) height_ok(rep *dash.Representation) bool {
-   switch f.Height {
-   case 0, *rep.Height:
-      return true
-   }
-   return false
-}
-
-func (f Filters) representation_ok(rep *dash.Representation) bool {
-   for _, filterVar := range f {
-      if !filterVar.bitrate_end_ok(rep) {
-         continue
-      }
-      if filterVar.BitrateStart > rep.Bandwidth {
-         continue
-      }
-      if !filterVar.height_ok(rep) {
-         continue
-      }
-      if !filterVar.id_ok(rep) {
-         continue
-      }
-      if !filterVar.language_ok(rep) {
-         continue
-      }
-      if !filterVar.role_ok(rep) {
-         continue
-      }
-      return true
-   }
-   return false
 }
