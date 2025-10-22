084676db637dd8ce7c808f36b2f66629fe15e2e7
27 <27@riseup.net>
2025-10-21 22:36:00 -0500

    new file:   SegmentBase.go

--- net.go
+++ net.go
@@ -12,6 +12,7 @@ import (
    "fmt"
    "io"
    "log"
+   "math"
    "math/big"
    "net/http"
    "net/url"
@@ -21,74 +22,217 @@ import (
    "time"
 )
 
-func (c *Config) segment_base(represent *dash.Representation) error {
-   if Threads != 1 {
-      return errors.New("SegmentBase Threads")
-   }
-   var media media_file
-   err := media.New(represent)
-   if err != nil {
-      return err
-   }
-   os_file, err := create(represent)
-   if err != nil {
-      return err
-   }
-   defer os_file.Close()
-   head := http.Header{}
-   head.Set("range", "bytes="+represent.SegmentBase.Initialization.Range)
-   data, err := get_segment(represent.BaseUrl[0], head)
-   if err != nil {
-      return err
-   }
-   data, err = media.initialization(data)
-   if err != nil {
-      return err
-   }
-   _, err = os_file.Write(data)
-   if err != nil {
-      return err
-   }
-   key, err := c.key(&media)
-   if err != nil {
-      return err
-   }
-   head.Set("range", "bytes="+represent.SegmentBase.IndexRange)
-   data, err = get_segment(represent.BaseUrl[0], head)
-   if err != nil {
-      return err
-   }
-   var file_file file.File
-   err = file_file.Read(data)
-   if err != nil {
-      return err
-   }
-   var progressVar progress
-   progressVar.set(len(file_file.Sidx.Reference))
-   var index index_range
-   err = index.Set(represent.SegmentBase.IndexRange)
-   if err != nil {
-      return err
-   }
-   for _, reference := range file_file.Sidx.Reference {
-      index[0] = index[1] + 1
-      index[1] += uint64(reference.Size())
-      head.Set("range", "bytes="+index.String())
-      data, err = get_segment(represent.BaseUrl[0], head)
-      if err != nil {
-         return err
+func (f *Filter) index(streams []*dash.Representation) int {
+   const penalty_factor = 2
+   min_score := math.MaxInt
+   best_stream := -1
+   for i, candidate := range streams {
+      if f.Codecs != "" {
+         if candidate.Codecs != nil {
+            if !strings.HasPrefix(*candidate.Codecs, f.Codecs) {
+               continue
+            }
+         }
       }
-      progressVar.next()
-      data, err = media.write_segment(data, key)
-      if err != nil {
-         return err
+      if f.Height >= 1 {
+         if candidate.Height != nil {
+            if *candidate.Height != f.Height {
+               continue
+            }
+         }
       }
-      _, err = os_file.Write(data)
+      if f.Id != "" {
+         if candidate.Id == f.Id {
+            return i
+         } else {
+            continue
+         }
+      }
+      if f.Lang != "" {
+         if candidate.GetAdaptationSet().Lang != f.Lang {
+            continue
+         }
+      }
+      if f.Role != "" {
+         if candidate.GetAdaptationSet().GetRole() != f.Role {
+            continue
+         }
+      }
+      var score int
+      if candidate.Bandwidth >= f.Bandwidth {
+         score = candidate.Bandwidth - f.Bandwidth
+      } else {
+         score = (f.Bandwidth - candidate.Bandwidth) * penalty_factor
+      }
+      if score < min_score {
+         min_score = score
+         best_stream = i
+      }
+   }
+   return best_stream
+}
+
+func (f *Filters) Filter(resp *http.Response, configVar *Config) error {
+   if resp.StatusCode != http.StatusOK {
+      var data strings.Builder
+      resp.Write(&data)
+      return errors.New(data.String())
+   }
+   defer resp.Body.Close()
+   data, err := io.ReadAll(resp.Body)
+   if err != nil {
+      return err
+   }
+   var mpd dash.Mpd
+   err = mpd.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   mpd.Set(resp.Request.URL)
+   represents := slices.SortedFunc(mpd.Representation(),
+      func(a, b *dash.Representation) int {
+         return a.Bandwidth - b.Bandwidth
+      },
+   )
+   for i, represent := range represents {
+      if i >= 1 {
+         fmt.Println()
+      }
+      fmt.Println(represent)
+   }
+   for _, target := range f.Values {
+      index := target.index(represents)
+      if index == -1 {
+         continue
+      }
+      represent := represents[index]
+      switch {
+      case represent.SegmentBase != nil:
+         err = configVar.segment_base(represent)
+      case represent.SegmentList != nil:
+         err = configVar.segment_list(represent)
+      case represent.SegmentTemplate != nil:
+         err = configVar.segment_template(represent)
+      }
       if err != nil {
          return err
       }
    }
    return nil
+}
+
+type Filters struct {
+   Values []Filter
+   set    bool
+}
+
+func (f *Filters) Set(input string) error {
+   if !f.set {
+      f.Values = nil
+      f.set = true
+   }
+   var value Filter
+   err := value.Set(input)
+   if err != nil {
+      return err
+   }
+   f.Values = append(f.Values, value)
+   return nil
+}
+
+func (f *Filters) String() string {
+   var out []byte
+   for i, value := range f.Values {
+      if i >= 1 {
+         out = append(out, ' ')
+      }
+      out = fmt.Append(out, "-f ", &value)
+   }
+   return string(out)
+}
+
+func (f *Filter) Set(input string) error {
+   for _, pair := range strings.Split(input, ",") {
+      key, value, found := strings.Cut(pair, "=")
+      if !found {
+         return errors.New("invalid pair format")
+      }
+      var err error
+      switch key {
+      case "b":
+         _, err = fmt.Sscan(value, &f.Bandwidth)
+      case "c":
+         f.Codecs = value
+      case "h":
+         _, err = fmt.Sscan(value, &f.Height)
+      case "i":
+         f.Id = value
+      case "l":
+         f.Lang = value
+      case "r":
+         f.Role = value
+      default:
+         err = errors.New("unknown key")
+      }
+      if err != nil {
+         return err
+      }
+   }
+   return nil
+}
+
+func (f *Filter) String() string {
+   var out []byte
+   if f.Bandwidth >= 1 {
+      out = fmt.Append(out, "b=", f.Bandwidth)
+   }
+   if f.Codecs != "" {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "c=", f.Codecs)
+   }
+   if f.Height >= 1 {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "h=", f.Height)
+   }
+   if f.Id != "" {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "i=", f.Id)
+   }
+   if f.Lang != "" {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "l=", f.Lang)
+   }
+   if f.Role != "" {
+      if out != nil {
+         out = append(out, ',')
+      }
+      out = fmt.Append(out, "r=", f.Role)
+   }
+   return string(out)
+}
+
+const FilterUsage = `b = bandwidth
+c = codecs
+h = height
+i = id
+l = lang
+r = role`
+
+type Filter struct {
+   Bandwidth int
+   Id        string
+   Height    int
+   Lang      string
+   Role      string
+   Codecs    string
 }
 
 func (c *Config) segment_list(represent *dash.Representation) error {
