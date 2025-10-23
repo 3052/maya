fe4e7ffbe2d7ae9f5ed53505955052e205a87be6
27 <27@riseup.net>
2025-10-22 21:40:42 -0500

    +func (c *Config) DownloadRepresentation(represent *dash.Representation) error {

--- net_test.go
+++ net_test.go
@@ -14,12 +14,12 @@ func point[T any](data T) *T {
 
 func TestIndex(t *testing.T) {
    streams := []*dash.Representation{
-      {Id: "v12", Bandwidth: 4618234, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)}, // Below target
-      {Id: "v16", Bandwidth: 4965335, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)}, // Below target
-      {Id: "v19", Bandwidth: 24155623, Codecs: point("hvc1.2.4.L150.90"), Height: point(2160)},// Wrong height
-      {Id: "v26", Bandwidth: 3189767, Codecs: point("dvh1.05.03"), Height: point(900)},    // Wrong height
-      {Id: "v5", Bandwidth: 11206136, Codecs: point("avc1.640028"), Height: point(1080)},   // Wrong codec (avc1)
-      {Id: "v8", Bandwidth: 8355097, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)},  // Above target
+      {Id: "v12", Bandwidth: 4618234, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)},  // Below target
+      {Id: "v16", Bandwidth: 4965335, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)},  // Below target
+      {Id: "v19", Bandwidth: 24155623, Codecs: point("hvc1.2.4.L150.90"), Height: point(2160)}, // Wrong height
+      {Id: "v26", Bandwidth: 3189767, Codecs: point("dvh1.05.03"), Height: point(900)},         // Wrong height
+      {Id: "v5", Bandwidth: 11206136, Codecs: point("avc1.640028"), Height: point(1080)},       // Wrong codec (avc1)
+      {Id: "v8", Bandwidth: 8355097, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)},   // Above target
    }
    expectedID := "v8"
    target := Filter{Bandwidth: 7_500_000}
