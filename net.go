52fd1e95889ffbe4fcbfa71b15d8bc5fa27b6b65
27 <27@riseup.net>
2025-10-31 21:57:05 -0500

    deleted:    pssh.go

--- net.go
+++ net.go
@@ -3,8 +3,10 @@ package net
 import (
    "41.neocities.org/dash"
    "41.neocities.org/drm/playReady"
+   "41.neocities.org/drm/widevine"
    "41.neocities.org/sofia"
    "bytes"
+   "encoding/base64"
    "encoding/hex"
    "errors"
    "fmt"
@@ -19,6 +21,183 @@ import (
    "strings"
    "time"
 )
+
+func (c *Config) get_media_requests(represent *dash.Representation) ([]media_request, error) {
+   switch {
+   case represent.SegmentTemplate != nil:
+      var segments []int
+      for rep := range represent.Representation() {
+         segments = slices.AppendSeq(segments, rep.Segment())
+      }
+      requests := make([]media_request, len(segments))
+      for i, segment := range segments {
+         address, err := represent.SegmentTemplate.Media.Url(represent, segment)
+         if err != nil {
+            return nil, err
+         }
+         requests[i] = media_request{url: address}
+      }
+      return requests, nil
+   case represent.SegmentBase != nil:
+      head := http.Header{}
+      head.Set("range", "bytes="+represent.SegmentBase.IndexRange)
+      data, err := get_segment(represent.BaseUrl[0], head)
+      if err != nil {
+         return nil, err
+      }
+      parsed, err := sofia.Parse(data)
+      if err != nil {
+         return nil, err
+      }
+      var index index_range
+      if err = index.Set(represent.SegmentBase.IndexRange); err != nil {
+         return nil, err
+      }
+      sidx, ok := sofia.FindSidx(parsed)
+      if !ok {
+         return nil, errors.New("sidx box not found in file")
+      }
+      requests := make([]media_request, len(sidx.References))
+      for i, reference := range sidx.References {
+         index[0] = index[1] + 1
+         index[1] += uint64(reference.ReferencedSize)
+         range_head := http.Header{}
+         range_head.Set("range", "bytes="+index.String())
+         requests[i] = media_request{
+            url: represent.BaseUrl[0], header: range_head,
+         }
+      }
+      return requests, nil
+   case represent.SegmentList != nil:
+      requests := make([]media_request, len(represent.SegmentList.SegmentUrl))
+      for i, segment := range represent.SegmentList.SegmentUrl {
+         requests[i] = media_request{url: segment.Media[0]}
+      }
+      return requests, nil
+   }
+   return []media_request{
+      {url: represent.BaseUrl[0]},
+   }, nil
+}
+
+type media_request struct {
+   url    *url.URL
+   header http.Header
+}
+
+func (m *media_file) initialization(data []byte) ([]byte, error) {
+   parsedInit, err := sofia.Parse(data)
+   if err != nil {
+      return nil, err
+   }
+   moov, ok := sofia.FindMoov(parsedInit)
+   if !ok {
+      return nil, errors.New("could not find 'moov' box in init file")
+   }
+   trak, ok := moov.Trak()
+   if !ok {
+      return nil, errors.New("could not find 'trak' in moov")
+   }
+   // THIS FIXES A/V SYNC WITH
+   // rakuten.tv
+   // BUT MIGHT BREAK THESE
+   // criterionchannel.com
+   // mubi.com
+   // paramountplus.com
+   // tubitv.com
+   trak.ReplaceEdts()
+   mdhd, ok := trak.Mdhd()
+   if !ok {
+      return nil, errors.New("could not find 'mdhd' in trak to get timescale")
+   }
+   m.timescale = uint64(mdhd.Timescale)
+   err = moov.Sanitize()
+   if err != nil {
+      return nil, err
+   }
+   m.key_id = trak.Tenc().DefaultKID[:]
+   if m.pssh == nil {
+      widevine_box, _ := moov.FindPssh(widevine_id)
+      m.pssh = widevine_box.Data
+      log.Println("MP4 PSSH", base64.StdEncoding.EncodeToString(m.pssh))
+   }
+   var finalMP4Data bytes.Buffer
+   for _, box := range parsedInit {
+      finalMP4Data.Write(box.Encode())
+   }
+   return finalMP4Data.Bytes(), nil
+}
+
+func (c *Config) widevine_key(media *media_file) ([]byte, error) {
+   if media.key_id == nil {
+      return nil, nil
+   }
+   private_key, err := os.ReadFile(c.PrivateKey)
+   if err != nil {
+      return nil, err
+   }
+   client_id, err := os.ReadFile(c.ClientId)
+   if err != nil {
+      return nil, err
+   }
+   var cdm widevine.Cdm
+   err = cdm.New(private_key, client_id, media.pssh)
+   if err != nil {
+      return nil, err
+   }
+   data, err := cdm.RequestBody()
+   if err != nil {
+      return nil, err
+   }
+   data, err = c.Send(data)
+   if err != nil {
+      return nil, err
+   }
+   var body widevine.ResponseBody
+   err = body.Unmarshal(data)
+   if err != nil {
+      return nil, err
+   }
+   block, err := cdm.Block(body)
+   if err != nil {
+      return nil, err
+   }
+   for container := range body.Container() {
+      if bytes.Equal(container.Id(), media.key_id) {
+         key := container.Key(block)
+         log.Printf("key %x", key)
+         var zero [16]byte
+         if !bytes.Equal(key, zero[:]) {
+            return key, nil
+         }
+      }
+   }
+   return nil, errors.New("widevine_key")
+}
+
+const widevine_urn = "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
+
+func (m *media_file) New(represent *dash.Representation) error {
+   for _, content := range represent.ContentProtection {
+      if content.SchemeIdUri == widevine_urn {
+         if content.Pssh != "" {
+            data, err := base64.StdEncoding.DecodeString(content.Pssh)
+            if err != nil {
+               return err
+            }
+            var box sofia.PsshBox
+            err = box.Parse(data)
+            if err != nil {
+               return err
+            }
+            m.pssh = box.Data
+            log.Println("MPD PSSH", base64.StdEncoding.EncodeToString(m.pssh))
+            break
+         }
+      }
+   }
+   return nil
+}
 
 func (f *Filters) Filter(resp *http.Response, configVar *Config) error {
    if resp.StatusCode != http.StatusOK {
@@ -225,6 +404,7 @@ type Filter struct {
    Role      string
    Codecs    string
 }
+
 type media_file struct {
    key_id    []byte // tenc
    pssh      []byte // pssh
@@ -234,62 +414,6 @@ type media_file struct {
 }
 
 var widevine_id, _ = hex.DecodeString("edef8ba979d64acea3c827dcd51d21ed")
-
-func (c *Config) get_media_requests(represent *dash.Representation) ([]media_request, error) {
-   switch {
-   case represent.SegmentList != nil:
-      requests := make([]media_request, len(represent.SegmentList.SegmentUrl))
-      for i, segment := range represent.SegmentList.SegmentUrl {
-         requests[i] = media_request{url: segment.Media[0]}
-      }
-      return requests, nil
-   case represent.SegmentTemplate != nil:
-      var segments []int
-      for rep := range represent.Representation() {
-         segments = slices.AppendSeq(segments, rep.Segment())
-      }
-      requests := make([]media_request, len(segments))
-      for i, segment := range segments {
-         address, err := represent.SegmentTemplate.Media.Url(represent, segment)
-         if err != nil {
-            return nil, err
-         }
-         requests[i] = media_request{url: address}
-      }
-      return requests, nil
-   case represent.SegmentBase != nil:
-      head := http.Header{}
-      head.Set("range", "bytes="+represent.SegmentBase.IndexRange)
-      data, err := get_segment(represent.BaseUrl[0], head)
-      if err != nil {
-         return nil, err
-      }
-      parsed, err := sofia.Parse(data)
-      if err != nil {
-         return nil, err
-      }
-      var index index_range
-      if err = index.Set(represent.SegmentBase.IndexRange); err != nil {
-         return nil, err
-      }
-      sidx, ok := sofia.FindSidx(parsed)
-      if !ok {
-         return nil, errors.New("sidx box not found in file")
-      }
-      requests := make([]media_request, len(sidx.References))
-      for i, reference := range sidx.References {
-         index[0] = index[1] + 1
-         index[1] += uint64(reference.ReferencedSize)
-         range_head := http.Header{}
-         range_head.Set("range", "bytes="+index.String())
-         requests[i] = media_request{
-            url: represent.BaseUrl[0], header: range_head,
-         }
-      }
-      return requests, nil
-   }
-   return nil, errors.New("unsupported segment type")
-}
 
 // segment can be VTT or anything
 func (m *media_file) write_segment(data, key []byte) ([]byte, error) {
@@ -420,11 +544,6 @@ func (c *Config) DownloadRepresentation(represent *dash.Representation) error {
 
    // Block and wait for the final status from the writer.
    return <-doneChan
-}
-
-type media_request struct {
-   url    *url.URL
-   header http.Header
 }
 
 func (c *Config) download_initialization(
