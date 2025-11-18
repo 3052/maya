379f8e53c9d8bb055591e5796c390082c6dd33b9
27 <27@riseup.net>
2025-11-18 17:13:05 -0600

    -   "41.neocities.org/dash"

--- net_test.go
+++ net_test.go
@@ -1,7 +1,6 @@
 package net
 
 import (
-   "41.neocities.org/dash"
    "testing"
    "time"
 )
@@ -61,30 +60,5 @@ func TestEtaCalculationWithUnixThrottle(t *testing.T) {
    } else {
       // This block should not be reached if the test runs for more than 1 second.
       t.Fatalf("Test setup error: Throttling condition was not met.")
-   }
-}
-
-func point[T any](data T) *T {
-   return &data
-}
-
-func TestIndex(t *testing.T) {
-   streams := []*dash.Representation{
-      {Id: "v12", Bandwidth: 4618234, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)},  // Below target
-      {Id: "v16", Bandwidth: 4965335, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)},  // Below target
-      {Id: "v19", Bandwidth: 24155623, Codecs: point("hvc1.2.4.L150.90"), Height: point(2160)}, // Wrong height
-      {Id: "v26", Bandwidth: 3189767, Codecs: point("dvh1.05.03"), Height: point(900)},         // Wrong height
-      {Id: "v5", Bandwidth: 11206136, Codecs: point("avc1.640028"), Height: point(1080)},       // Wrong codec (avc1)
-      {Id: "v8", Bandwidth: 8355097, Codecs: point("hvc1.2.4.L120.90"), Height: point(1080)},   // Above target
-   }
-   expectedID := "v8"
-   target := Filter{Bandwidth: 7_500_000}
-   index := target.index(streams)
-   if index == -1 {
-      t.Fatal("FindGoodStream returned a nil result without an error.")
-   }
-   result := streams[index]
-   if result.Id != expectedID {
-      t.Errorf("Incorrect stream selected. Expected Id '%s', but got '%s'", expectedID, result.Id)
    }
 }
