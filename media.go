987e3a983b38e67042d4c90a01443fe87a9d50ae
27 <27@riseup.net>
2025-11-28 23:04:26 -0600

    +	41.neocities.org/sofia v1.11.3

--- media.go
+++ media.go
@@ -10,99 +10,6 @@ import (
    "strings"
 )
 
-func (m *MediaFile) configureProtection(rep *dash.Representation) error {
-   for _, protect := range rep.GetContentProtection() {
-      switch strings.ToLower(protect.SchemeIdUri) {
-      case protectionURN:
-         // 1. get `default_KID` from MPD
-         // https://ctv.ca MPD is missing PSSH
-         data, err := protect.GetDefaultKID()
-         if err != nil {
-            return err
-         }
-         if data != nil {
-            m.key_id = data
-            log.Printf("key ID %x", m.key_id)
-         }
-      case widevineURN:
-         // 2. check if MPD has PSSH, check if PSSH has content ID
-         // https://hulu.com poisons the PSSH so we only want content ID
-         data, err := protect.GetPSSH()
-         if err != nil {
-            return err
-         }
-         if data != nil {
-            var pssh_box sofia.PsshBox
-            err = pssh_box.Parse(data)
-            if err != nil {
-               return err
-            }
-            var pssh_data widevine.PsshData
-            err = pssh_data.Unmarshal(pssh_box.Data)
-            if err != nil {
-               return err
-            }
-            if pssh_data.ContentID != nil {
-               m.content_id = pssh_data.ContentID
-               log.Printf("DASH content ID %x", m.content_id)
-            }
-         }
-      }
-   }
-   return nil
-}
-
-func (m *MediaFile) processInit(data []byte) ([]byte, error) {
-   parsedInit, err := sofia.Parse(data)
-   if err != nil {
-      return nil, err
-   }
-   moov, ok := sofia.FindMoov(parsedInit)
-   if !ok {
-      return nil, sofia.Missing("moov")
-   }
-   // 3. check if initialization has PSSH, check if PSSH has content ID
-   if wvBox, ok := moov.FindPssh(widevineID); ok {
-      var pssh_data widevine.PsshData
-      err = pssh_data.Unmarshal(wvBox.Data)
-      if err != nil {
-         return nil, err
-      }
-      if pssh_data.ContentID != nil {
-         m.content_id = pssh_data.ContentID
-         log.Printf("MP4 content ID %x", m.content_id)
-      }
-   }
-   trak, ok := moov.Trak()
-   if !ok {
-      return nil, sofia.Missing("trak")
-   }
-   trak.ReplaceEdts()
-   mdia, ok := trak.Mdia()
-   if !ok {
-      return nil, sofia.Missing("mdia")
-   }
-   mdhd, ok := mdia.Mdhd()
-   if !ok {
-      return nil, sofia.Missing("mdhd")
-   }
-   m.timescale = uint64(mdhd.Timescale)
-   err = moov.Sanitize()
-   if err != nil {
-      return nil, err
-   }
-   var buf bytes.Buffer
-   for _, box := range parsedInit {
-      buf.Write(box.Encode())
-   }
-   return buf.Bytes(), nil
-}
-
-const (
-   protectionURN = "urn:mpeg:dash:mp4protection:2011"
-   widevineURN   = "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
-)
-
 func (m *MediaFile) processSegment(data, key []byte, p *progress) ([]byte, error) {
    parsed, err := sofia.Parse(data)
    if err != nil {
@@ -188,3 +95,110 @@ type MediaFile struct {
    key_id     []byte
    content_id []byte
 }
+func (m *MediaFile) configureProtection(rep *dash.Representation) error {
+   for _, protect := range rep.GetContentProtection() {
+      switch strings.ToLower(protect.SchemeIdUri) {
+      case protectionURN:
+         // 1. get `default_KID` from MPD
+         // https://ctv.ca MPD is missing PSSH
+         data, err := protect.GetDefaultKID()
+         if err != nil {
+            return err
+         }
+         if data != nil {
+            m.key_id = data
+            log.Printf("key ID %x", m.key_id)
+         }
+      case widevineURN:
+         // 2. check if MPD has PSSH, check if PSSH has content ID
+         // https://hulu.com poisons the PSSH so we only want content ID
+         data, err := protect.GetPSSH()
+         if err != nil {
+            return err
+         }
+         if data != nil {
+            var pssh_box sofia.PsshBox
+            err = pssh_box.Parse(data)
+            if err != nil {
+               return err
+            }
+            var pssh_data widevine.PsshData
+            err = pssh_data.Unmarshal(pssh_box.Data)
+            if err != nil {
+               return err
+            }
+            if pssh_data.ContentID != nil {
+               m.content_id = pssh_data.ContentID
+               log.Printf("DASH content ID %x", m.content_id)
+            }
+         }
+      }
+   }
+   return nil
+}
+func (m *MediaFile) processInit(data []byte) ([]byte, error) {
+   parsedInit, err := sofia.Parse(data)
+   if err != nil {
+      return nil, err
+   }
+   // 1 pssh
+   moov, ok := sofia.FindMoov(parsedInit)
+   if !ok {
+      return nil, sofia.Missing("moov")
+   }
+   if wvBox, ok := moov.FindPssh(widevineID); ok {
+      var pssh_data widevine.PsshData
+      err = pssh_data.Unmarshal(wvBox.Data)
+      if err != nil {
+         return nil, err
+      }
+      if pssh_data.ContentID != nil {
+         m.content_id = pssh_data.ContentID
+         log.Printf("MP4 content ID %x", m.content_id)
+      }
+   }
+   moov.RemovePssh()
+   // 2 edts
+   trak, ok := moov.Trak()
+   if !ok {
+      return nil, sofia.Missing("trak")
+   }
+   trak.RemoveEdts()
+   // 3 mdhd
+   mdia, ok := trak.Mdia()
+   if !ok {
+      return nil, sofia.Missing("mdia")
+   }
+   mdhd, ok := mdia.Mdhd()
+   if !ok {
+      return nil, sofia.Missing("mdhd")
+   }
+   m.timescale = uint64(mdhd.Timescale)
+   // 4 stsd
+   minf, ok := mdia.Minf()
+   if !ok {
+      return nil, sofia.Missing("minf")
+   }
+   stbl, ok := minf.Stbl()
+   if !ok {
+      return nil, sofia.Missing("stbl")
+   }
+   stsd, ok := stbl.Stsd()
+   if !ok {
+      return nil, sofia.Missing("stsd")
+   }
+   err = stsd.UnprotectAll()
+   if err != nil {
+      return nil, err
+   }
+   var buf bytes.Buffer
+   for _, box := range parsedInit {
+      buf.Write(box.Encode())
+   }
+   return buf.Bytes(), nil
+}
+
+const (
+   protectionURN = "urn:mpeg:dash:mp4protection:2011"
+   widevineURN   = "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
+)
