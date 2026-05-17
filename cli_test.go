329807f58b09c23eaf753a0466b578857ca03c86
27 <27@riseup.net>
2026-05-16 19:52:55 -0500

    deleted:    research/cli.go

--- cli_test.go
+++ cli_test.go
@@ -5,12 +5,23 @@ import (
    "testing"
 )
 
-func TestPrintFlagSetString(t *testing.T) {
-   var fs FlagSet
-   var fDebug, fFile Flag
+func TestFlagSet_String(t *testing.T) {
+   fmt.Println("--- Multiple Groups ---")
+   var fsMulti FlagSet
 
-   fs.Add(&fDebug, "enable debug mode")
-   fs.AddValue(&fFile, "set output file")
+   fsMulti.AddGroup("help-menu", false, 1)
+   fsMulti.AddGroup("verbose-logging-output", false, 1)
 
-   fmt.Println(fs.String())
+   fsMulti.AddGroup("database-username", true, 2)
+   fsMulti.AddGroup("database-password", true, 2)
+
+   fmt.Println(fsMulti.String())
+
+   fmt.Println("\n--- Single Group ---")
+   var fsSingle FlagSet
+
+   fsSingle.Add("help-menu", false)
+   fsSingle.Add("target-url", true)
+
+   fmt.Println(fsSingle.String())
 }
