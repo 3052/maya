1c8b5cf4af4641e868b0bda8c37bb78b6558e1e9
27 <27@riseup.net>
2026-05-24 13:05:02 -0500

    deleted:    research/flag_test.go

--- research/flag.go
+++ research/flag.go
@@ -1,104 +1,38 @@
-package flag
+package main
 
 import (
+   "flag"
    "fmt"
-   "strconv"
-   "strings"
 )
 
-// Value is the interface to the dynamic value stored in a flag.
-type Value interface {
-   String() string
-   Set(string) error
+type hello struct {
+   value string
 }
 
-// StringValue contains a string flag's value and tracks if it has been set.
-type StringValue struct {
-   Value string
-   IsSet bool
-}
-
-// String returns the string representation of the value.
-func (s *StringValue) String() string {
-   return s.Value
-}
-
-// Set parses the string and updates the value state.
-func (s *StringValue) Set(input string) error {
-   s.Value = input
-   s.IsSet = true
+func (h *hello) Set(value string) error {
+   h.value = value
    return nil
 }
 
-// IntValue contains an int flag's value and tracks if it has been set.
-type IntValue struct {
-   Value int
-   IsSet bool
+func (h *hello) String() string {
+   return h.value
 }
 
-// String returns the string representation of the value.
-func (i *IntValue) String() string {
-   return strconv.Itoa(i.Value)
-}
-
-// Set parses the integer string and updates the value state.
-func (i *IntValue) Set(input string) error {
-   var err error
-   i.Value, err = strconv.Atoi(input)
-   if err != nil {
-      return err
-   }
-   i.IsSet = true
-   return nil
-}
-
-// Flag represents the state of a single flag.
-type Flag struct {
-   Name  string
-   Usage string
-   Value Value
-}
-
-// FlagSet represents a set of defined flags.
-type FlagSet []*Flag
-
-// Lookup returns the Flag structure of the named flag, returning nil if none exists.
-func (fs FlagSet) Lookup(name string) *Flag {
-   for _, f := range fs {
-      if f.Name == name {
-         return f
+func (h *hello) set() bool {
+   var set bool
+   flag.Visit(func(f *flag.Flag) {
+      if f.Value == h {
+         set = true
       }
-   }
-   return nil
+   })
+   return set
 }
 
-// Parse parses flag definitions from the argument list.
-// It only supports "key", "key=", and "key=value" formats.
-func (fs FlagSet) Parse(arguments []string) error {
-   for _, s := range arguments {
-      // strings.Cut perfectly handles all three scenarios:
-      // "key=value" -> name: "key", value: "value"
-      // "key="      -> name: "key", value: ""
-      // "key"       -> name: "key", value: "" (when separator is not found)
-      name, value, _ := strings.Cut(s, "=")
-
-      // Find the flag using Lookup
-      f := fs.Lookup(name)
-      if f == nil {
-         return fmt.Errorf("flag provided but not defined: %s", name)
-      }
-
-      // Delegate the parsing and state updating to the interface method
-      if err := f.Value.Set(value); err != nil {
-         return fmt.Errorf("invalid value %q for flag %s: %v", value, name, err)
-      }
-   }
-   return nil
-}
-
-// PrintDefaults prints a usage message showing all defined flags.
-func (fs FlagSet) PrintDefaults() {
-   for _, f := range fs {
-      fmt.Printf("%s value\n\t%s (default %s)\n", f.Name, f.Usage, f.Value.String())
-   }
+func main() {
+   var alfa hello
+   var bravo hello
+   flag.Var(&alfa, "alfa", "usage")
+   flag.Var(&bravo, "bravo", "usage")
+   flag.Parse()
+   fmt.Println(alfa.set(), bravo.set())
 }
